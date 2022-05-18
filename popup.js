5d92b0fa01f5001b0062f420ef4902cbd60466df
TerryLee <Terry.lee@ehsn.com.tw>
2022-05-18 16:06:57 +0800

    update

--- popup.js
+++ popup.js
@@ -17,6 +17,7 @@
 
 // The body of this function will be execuetd as a content script inside the current page
 // 1. 改變 body 背景顏色
+/*
 (() => {
   function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
@@ -58,8 +59,10 @@
   buttonWrap.appendChild(fragment)
 
 })();
+*/
 
 // 2. 打API 拿資料呈現
+/*
 (() => {
   function success (res) {
     const lists = res
@@ -89,8 +92,10 @@
     .then(res => { return res.json() })
     .then(success)
 })();
+*/
 
 // 3. 取當前tab cookie
+/*
 (() => {
   const getCookieButton = document.querySelector('.js-getCookie')
   const cookieResult = document.querySelector('.cookie-result')
@@ -131,3 +136,114 @@
     await showCookieResult()
   })
 })();
+*/
+
+// 改成透過 background.js 拿當前 tab 的 cookie, 開啟 popup 直接就將 cookie 寫入畫面
+/*
+chrome.storage.sync.get('currentCookies', (result) => {
+  try {
+    const { currentCookies } = result
+    console.log('currentCookies: ', currentCookies);
+    const cookieResult = document.querySelector('.cookie-result')
+    const fragment = document.createDocumentFragment()
+    // 做新的資料結構
+    const currentCookiesTable = currentCookies
+      .split(';')
+      .reduce((cookieTable, fullCookie) => {
+          const [key, value] = fullCookie.split('=')
+          cookieTable[key] = value
+          return cookieTable
+      }, {})
+    // 做畫面
+    for (const keyName in currentCookiesTable) {
+      const cookieValue = currentCookiesTable[keyName]
+      const div = document.createElement('div')
+      div.innerHTML = `${keyName}=${cookieValue}`
+      fragment.appendChild(div)
+    }
+    const now = new Date().toString()
+    const nowDiv = document.createElement('div')
+    nowDiv.style.marginTop = '5px'
+    nowDiv.style.borderTop = '1px solid'
+    nowDiv.textContent = `最後更新時間: ${now}`
+    fragment.appendChild(nowDiv)
+
+    cookieResult.appendChild(fragment)
+  } catch (error) {
+    console.log(error);
+  }
+})
+*/
+
+(() => {
+  const saveBtn = document.querySelector('#save-btn')
+
+  saveBtn.addEventListener('click', async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: () => {
+        try {
+          const messageBlocks = document.querySelectorAll('[aria-live="polite"] > [jsname]'),
+            roomName = document.querySelector('.u6vdEc.ouH3xe').textContent
+          let dataText = ''
+
+          function saveData (data) {
+            const now = new Date(),
+              yyyy = now.getFullYear(),
+              mm = now.getMonth() + 1,
+              dd = now.getDate(),
+              textToBLOB = new Blob([data], { type: 'text/plain' }),
+              sFileName = `${yyyy}-${mm}-${dd}-roomId-${roomName}.txt`;
+
+            let newLink = document.createElement("a");
+            newLink.download = sFileName;
+
+            if (window.webkitURL != null) {
+                newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+            } else {
+                newLink.href = window.URL.createObjectURL(textToBLOB);
+                newLink.style.display = "none";
+                document.body.appendChild(newLink);
+            }
+            newLink.click();
+          }
+          // var messageHashTable = new Map()
+
+          messageBlocks.forEach((el, idx) => {
+            const name = el.querySelector('div:nth-child(1) > [class]').textContent,
+                senderId = el.getAttribute('data-sender-id'),
+                formateTime = el.querySelector('div:nth-child(1) > div:nth-child(2)').textContent,
+                timestamp = el.getAttribute('data-timestamp'),
+                hashKey = `${senderId}_${timestamp}`,
+                messages = [];
+
+            el.querySelector('[jsname]').querySelectorAll('div').forEach(_el => {
+              messages.push(_el.textContent.replace(/\r?\n/g, '\\r\\n'))
+            })
+
+            dataText += (
+              `${name},${senderId},${timestamp},${messages}` +
+              '\r\n'
+            )
+
+            // messageHashTable.set(hashKey, {
+            //     order: idx,
+            //     name,
+            //     formateTime,
+            //     timestamp,
+            //     messages
+            // })
+
+          })
+          // console.log(dataText);
+          saveData(dataText)
+        } catch (error) {
+          alert('您不在 google meet 聊天室內。')
+        }
+      },
+    });
+  })
+})()
+
